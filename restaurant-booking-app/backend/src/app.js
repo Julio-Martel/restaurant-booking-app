8e147f5e62e7802d70d16b34fff263205eae6258
Julio-Martel <julioramdom123@gmail.com>
2026-04-27 01:29:02 -0300

    fix: rutas de autorizacion

--- restaurant-booking-app/backend/src/app.js
+++ restaurant-booking-app/backend/src/app.js
@@ -6,8 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth/', authRoutes);
 app.use('/api/admin/', adminRoutes);
-
 
 export default app;
