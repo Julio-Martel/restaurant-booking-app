4efcabb3a68f8ecfc7a0a8666509072ca3ca4572
Julio-Martel <julioramdom123@gmail.com>
2026-04-29 20:15:32 -0300

    feat: getRestaurantes

--- restaurant-booking-app/backend/src/models/usuario.model.js
+++ restaurant-booking-app/backend/src/models/usuario.model.js
@@ -1,8 +1,8 @@
 import db from '../config/db.js';
 
-const obtenerUsuario = async(email,pass) => {
+const obtenerUsuario = async(email) => {
     const [rows] = await db.query(`SELECT * FROM Usuarios
-        WHERE email = ? AND pass = ?`,[email,pass]);
+        WHERE email = ?`,[email]);
 
     return rows[0];
 
@@ -16,7 +16,32 @@ const comprobarMailExistente = async(email) => {
     return rows[0];
 }
 
-export {obtenerUsuario, comprobarMailExistente}
+const agregarUsuario = async(nombre,email,pass,rol) => {
+    const [rows] = await db.query(`INSERT INTO Usuarios(nombre,email,pass,rol)
+        VALUES(?,?,?,?)`,[nombre,email,pass,rol]);
+
+    return rows[0];
+}
+
+const obtenerUsuarios = async() => {
+    const [resultados] = await db.query(`SELECT * FROM Usuarios`);
+
+    return resultados;
+}
+
+const eliminarUsuario = async(id) => {
+    const [rows] = await db.query(`DELETE FROM Usuarios WHERE id = ?`,[id]);
+
+    return rows;
+}
+
+export {
+    obtenerUsuario, 
+    obtenerUsuarios,
+    comprobarMailExistente,
+    agregarUsuario,
+    eliminarUsuario
+}
 
 
 
