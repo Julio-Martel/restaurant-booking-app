bad68f65dd124876852ed6da0e3378f0180e025e
Julio-Martel <julioramdom123@gmail.com>
2026-04-30 19:00:27 -0300

    fix: next()

--- restaurant-booking-app/backend/src/middlewares/permisos.middlewares.js
+++ restaurant-booking-app/backend/src/middlewares/permisos.middlewares.js
@@ -5,7 +5,17 @@ const permisoAdmin = (req,res,next) => {
     }
 
     next();
-
 }
 
-export {permisoAdmin};
+const permisoCliente = (req,res,next) => {
+    if(req.user.rol !== 'cliente'){
+        return res.send('Solo el cliente puede hacer reservas.')
+    }
+
+    next();
+}
+
+export {
+    permisoAdmin,
+    permisoCliente
+};
