b0fa8911c3bac80670c4a678a98465c37b690aee
Julio-Martel <julioramdom123@gmail.com>
2026-05-03 18:19:15 -0300

    cambio

--- restaurant-booking-app/backend/src/app.js
+++ restaurant-booking-app/backend/src/app.js
@@ -1,5 +1,6 @@
 import adminRoutes from './routes/admin.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import authRoutes from './middlewares/auth.routes.js';
+import clienteRoutes from './routes/clientes.routes.js';
 import express from 'express';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth/', authRoutes);
-app.use('/api/admin/', adminRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/cliente', clienteRoutes);
 
 export default app;
