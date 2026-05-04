8b036e936b14a3f423164ad65dd6c70a0851562f
Julio-Martel <julioramdom123@gmail.com>
2026-05-04 01:54:06 -0300

    feat: dueñosRoutes

--- restaurant-booking-app/backend/src/app.js
+++ restaurant-booking-app/backend/src/app.js
@@ -1,6 +1,7 @@
 import adminRoutes from './routes/admin.routes.js';
 import authRoutes from './middlewares/auth.routes.js';
 import clienteRoutes from './routes/clientes.routes.js';
+import dueñoRoutes from './controllers/dueño.routes.js';
 import express from 'express';
 
 const app = express();
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/api/auth/', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/cliente', clienteRoutes);
+app.use('/api/dueño', dueñoRoutes);
 
 export default app;
