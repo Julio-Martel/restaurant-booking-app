0aa9566abbaf126ae94e19ac3d798823c80ddac7
Julio-Martel <julioramdom123@gmail.com>
2026-05-14 19:20:15 -0300

    fix: cambia la importacion de crear reserva

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { authToken } from '../middlewares/auth.middleware.js';
 import { permisoCliente } from '../middlewares/permisos.middlewares.js';
 import { getRestaurantes } from '../controllers/restaurante.model.js';
-import { verRestaurantes } from '../controllers/cliente.controllers.js';
-import { createReserva } from '../models/reservas.model.js';
+import { verRestaurantes, crearReserva } from '../controllers/cliente.controllers.js';
+
 
 const clienteRoutes = express.Router();
 
 clienteRoutes.get('/search', authToken, permisoCliente, verRestaurantes);
-clienteRoutes.post('/', authToken, permisoCliente, createReserva);
+clienteRoutes.post('/', authToken, permisoCliente, crearReserva);
 
 export default clienteRoutes;
