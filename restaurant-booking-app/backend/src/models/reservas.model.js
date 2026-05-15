63636f89df79b144f8776ff0016d991f37eaced4
Julio-Martel <julioramdom123@gmail.com>
2026-05-15 15:35:01 -0300

    feat: control fecha horario

--- restaurant-booking-app/backend/src/models/reservas.model.js
+++ restaurant-booking-app/backend/src/models/reservas.model.js
@@ -6,10 +6,18 @@ const createReserva = async(fecha_hora,cantidad_personas,estado,id_cliente,id_re
 
     const [resultadosRestaurantes] = await db.query(`SELECT * FROM Restaurantes WHERE id = ?`, [id_restaurante]);
 
+    const [fechaOcupadaEstado] = await db.query(`SELECT * FROM Reservas WHERE fecha_hora = ? OR fecha_hora > ?`, [fecha_hora]);
+
     if(!clienteExiste || !resultadosRestaurantes){
         return res.status(404).json({
             mensaje: "Cliente o restaurante inexistente"
         });
+    }
+
+    if(fechaOcupadaEstado.length !== 0){
+        return res.status(400).json({
+            mensaje:'No puede utilizar fechas pasadas o la actual'
+        })
     }
 
     const [reservaCreada] = await db.query(`INSERT INTO Reservas(fecha_hora,cantidad_personas,estado,id_cliente,id_restaurante)
@@ -26,6 +34,5 @@ const createReserva = async(fecha_hora,cantidad_personas,estado,id_cliente,id_re
     return reservaCreada;
 
 }
-
 
 export {createReserva};
