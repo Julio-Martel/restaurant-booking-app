7d9d15415e837f3ccd3289d6c171c0b013704c12
Julio-Martel <julioramdom123@gmail.com>
2026-05-17 16:43:40 -0300

    feat: nueva ruta de borrado de reservas

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { authToken } from '../middlewares/auth.middleware.js';
 import { permisoCliente } from '../middlewares/permisos.middlewares.js';
 import { getRestaurantes } from '../controllers/restaurante.model.js';
-import { verRestaurantes, crearReserva, verTodasTusReservas} from '../controllers/cliente.controllers.js';
+import { verRestaurantes, crearReserva, verTodasTusReservas, borrarReserva} from '../controllers/cliente.controllers.js';
 
 
 const clienteRoutes = express.Router();
@@ -10,5 +10,6 @@ const clienteRoutes = express.Router();
 clienteRoutes.get('/search', authToken, permisoCliente, verRestaurantes);
 clienteRoutes.post('/', authToken, permisoCliente, crearReserva);
 clienteRoutes.get('/reservas', authToken, permisoCliente, verTodasTusReservas);
+clienteRoutes.delete('/:id', authToken, permisoCliente,borrarReserva)
 
 export default clienteRoutes;
