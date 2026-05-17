bf28ffbcb88abffc345f783b01583e1b19947e73
Julio-Martel <julioramdom123@gmail.com>
2026-05-17 14:07:08 -0300

    feat: nueva ruta

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { authToken } from '../middlewares/auth.middleware.js';
 import { permisoCliente } from '../middlewares/permisos.middlewares.js';
 import { getRestaurantes } from '../controllers/restaurante.model.js';
-import { verRestaurantes, crearReserva } from '../controllers/cliente.controllers.js';
+import { verRestaurantes, crearReserva, verTodasTusReservas} from '../controllers/cliente.controllers.js';
 
 
 const clienteRoutes = express.Router();
 
 clienteRoutes.get('/search', authToken, permisoCliente, verRestaurantes);
 clienteRoutes.post('/', authToken, permisoCliente, crearReserva);
-clienteRoutes.get('/reservas', authToken, permisoCliente)
+clienteRoutes.get('/reservas', authToken, permisoCliente, verTodasTusReservas);
 
 export default clienteRoutes;
