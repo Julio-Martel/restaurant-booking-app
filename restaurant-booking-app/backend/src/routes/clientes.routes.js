7bab1032e80dfc2148619dbfc41ee7c1b8ee3ec9
Julio-Martel <julioramdom123@gmail.com>
2026-05-18 11:54:33 -0300

    refactor: agrega controller de cancelar la reserva

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { authToken } from '../middlewares/auth.middleware.js';
 import { permisoCliente } from '../middlewares/permisos.middlewares.js';
 import { getRestaurantes } from '../controllers/restaurante.model.js';
-import { verRestaurantes, crearReserva, verTodasTusReservas, borrarReserva} from '../controllers/cliente.controllers.js';
+import { verRestaurantes, crearReserva, verTodasTusReservas, cancelarReserva} from '../controllers/cliente.controllers.js';
 
 const clienteRoutes = express.Router();
 
@@ -16,10 +16,7 @@ clienteRoutes.post('/', authToken, permisoCliente, crearReserva);
 clienteRoutes.get('/reservas', authToken, permisoCliente, verTodasTusReservas);
 
 //CANCELAR UNA RESERVA
-clienteRoutes.patch('/:id', authToken, permisoCliente, /* agregar aqui el controller de cancelar la reserva */ );
+clienteRoutes.patch('/:id', authToken, permisoCliente, cancelarReserva);
 
-
-
-//clienteRoutes.delete('/:id', authToken, permisoCliente,borrarReserva)
 
 export default clienteRoutes;
