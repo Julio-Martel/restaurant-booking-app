7fbe8fca231708c56221fce7155dcb5d4bed9af9
Julio-Martel <julioramdom123@gmail.com>
2026-05-18 11:25:56 -0300

    feat: agrega comentarios

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -6,9 +6,17 @@ import { verRestaurantes, crearReserva, verTodasTusReservas, borrarReserva} from
 
 const clienteRoutes = express.Router();
 
+//EL CLIENTE PODRA VER TODOS LOS RESTAURANTES
 clienteRoutes.get('/search', authToken, permisoCliente, verRestaurantes);
+
+//EL CLIENTE PODRA VER TODAS SUS RESERVAS
 clienteRoutes.post('/', authToken, permisoCliente, crearReserva);
+
+//EL CLIENTE PODRA VER TODAS SUS RESERVAS
 clienteRoutes.get('/reservas', authToken, permisoCliente, verTodasTusReservas);
+
+
+
 //clienteRoutes.delete('/:id', authToken, permisoCliente,borrarReserva)
 
 export default clienteRoutes;
