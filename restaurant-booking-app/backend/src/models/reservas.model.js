2d46f39893d5593210ad8a07f89ad1161dc2e9e2
Julio-Martel <julioramdom123@gmail.com>
2026-05-19 16:14:19 -0300

    se eliminado el comentario de autoayuda

--- restaurant-booking-app/backend/src/models/reservas.model.js
+++ restaurant-booking-app/backend/src/models/reservas.model.js
@@ -53,45 +53,11 @@ const createReserva = async(data) => {
         throw new Error(`NO HAY LUGARES`)
     }
 
-    /*
-    
-        cantidad_total = 50;
-        
-        yo_reservo = 40;
-
-        si libres = cantidad_total - total_en_la_bd;
-
-        si yo reservo > libres entonces corto la funcion,
-        pero ahora digamos,
-        si ya hay reservas por ejemplo, asignamos los 40
-
-
-        >>>> libres = cantidad_total - total_en_la_bd,
-                = 50 - 40
-        entonces 
-        libres = 10(nueva cantidad al hjacer otra reserva)
-
-        yo_reservo = 20
-
-        si yo_reservo > libres entonces no hay lugares
-        como 20 > 10 entonces se termina,
-
-        pero si ingreso 5 por ej
-
-        entonces ahi lo guardo
-
-        tota en la bd seria de 45
-
-    
-    */
-
-
-
-
     const [reservaCreada] = await db.query(
         `INSERT INTO Reservas(
             fecha_hora,
             cantidad_personas,
+            estado,            
             id_cliente,
             id_restaurante
         )
@@ -99,6 +65,7 @@ const createReserva = async(data) => {
         [
             fecha_hora,
             cantidad_personas,
+            'pendiente',
             id_cliente, 
             id_restaurante
         ]
