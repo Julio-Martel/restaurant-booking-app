26081a152ecdb115aa3f3d66f89353490353384d
Julio-Martel <julioramdom123@gmail.com>
2026-05-22 20:13:36 -0300

    se han hechos cambios en las rutas

--- restaurant-booking-app/backend/src/routes/clientes.routes.js
+++ restaurant-booking-app/backend/src/routes/clientes.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { authToken } from '../middlewares/auth.middleware.js';
 import { permisoCliente } from '../middlewares/permisos.middlewares.js';
-import { getRestaurantes } from '../controllers/restaurante.model.js';
 import { verRestaurantes, crearReserva, verTodasTusReservas, cancelarReserva} from '../controllers/cliente.controllers.js';
 
 const clienteRoutes = express.Router();
