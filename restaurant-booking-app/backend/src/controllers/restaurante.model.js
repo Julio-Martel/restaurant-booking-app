c08496fa4d16b2337e6f1c2d915e66a6847e8659
Julio-Martel <julioramdom123@gmail.com>
2026-05-23 18:23:31 -0300

    se ha arreglo el tema de la consulta dinamica de actualizar el update de sql

--- restaurant-booking-app/backend/src/controllers/restaurante.model.js
+++ restaurant-booking-app/backend/src/controllers/restaurante.model.js
@@ -38,12 +38,9 @@ const createRestaurante = async(nombre, direccion, capacidad, id_usuario) => {
 
 const updateRestaurante = async(id,data) => {
 
-    /*ARREGLAR EL PORQUE SOLO SI INGRESO UN DATO NO ME DEJA CONTINUAR Y EL PORQUE 
-    TIRA ERROR DEL SERVIDOR*/
-
     const datosParaActualizar = [];
     const valores = [];
-    // ESTO CONVIERTE UN ARREGLO A FORMATO TEXTO
+
     const [restauranteEncontrado] = await db.query(`SELECT id FROM Restaurantes WHERE id = ?`,[id]);
     
     console.log(data.nombre,data.direccion,data.id_usuario);
@@ -70,14 +67,11 @@ const updateRestaurante = async(id,data) => {
         datosParaActualizar.push(data.id_usuario);
     }
 
-  const datosParaActualizarFormatoTexto = datosParaActualizar.join(" ") 
-
-    console.log(typeof datosParaActualizarFormatoTexto)
-
+  const datosParaActualizarFormatoTexto = datosParaActualizar.join(" , ") 
 
     if(datosParaActualizarFormatoTexto !== undefined){
         const [resultado] = await db.query(`UPDATE Restaurantes SET ${datosParaActualizarFormatoTexto} WHERE id = ?`,
-            [valores, id])
+            [...valores, id])
     
         return resultado;      
     }
